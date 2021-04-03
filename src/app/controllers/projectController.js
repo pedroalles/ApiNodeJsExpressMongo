1e438c872aa96ba3ed4c0574310e37cb4f6f7bb4
Pedro Alles <pedroalles@gmail.com>
2021-04-03 18:52:38 -0300

    CRUD started

--- src/app/controllers/projectController.js
+++ src/app/controllers/projectController.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const authMiddleware = require('../middlewares/auth');
 
+const Project = require('../models/project.js');
+const Task = require('../models/tasks.js');
+
 const router = express.Router();
 router.use(authMiddleware);
 
-router.get('/', (req, res) => {
-    res.send({ ok: true, user: req.userId});
+router.get('/', async (req, res) => {
+    res.send({user: req.userId});
+});
+
+router.get('/:projectId', async (req, res) => {
+    res.send({user: req.userId});
+});
+
+router.post('/', async (req, res) => {
+    res.send({ ok:true, user: req.userId});
+});
+
+router.put('/:projectId', async (req, res) => {
+    res.send({user: req.userId});
+});
+
+router.delete('/:projectId', async (req, res) => {
+    res.send({user: req.userId});
 });
 
 module.exports = app => app.use('/projects', router);
