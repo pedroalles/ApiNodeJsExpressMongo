af74a31bfeadadb92a12a6b0cb168932f3624ec5
Pedro Alles <pedroalles@gmail.com>
2021-04-03 16:53:05 -0300

    Changed reset password function

--- src/app/controllers/authController.js
+++ src/app/controllers/authController.js
@@ -16,7 +16,7 @@ function generateToken(params = {})
     });
 }
 
-router.post('/register', async (req, res) => {
+router.post('/register', async (req, res) => { 
 
     const { email } = req.body;
 
@@ -97,6 +97,36 @@ router.post('/forgot_password', async(req, res) => {
         console.log(err);
         res.status(400).send({ error: 'Erro on forgot password, try again'});
     }
-})
+});
+
+router.post('/reset_password', async (req, res) => {
+    const { email, token, password } = req.body;
+
+    try{
+        const user = await User.findOne( { email })
+            .select('+passwordResetToken passwordResetExpires');
+
+        if(!user)
+            return res.status(400).send({ error: 'User not found!'});
+
+        if(token !== user.passwordResetToken)
+            return res.status(400).send({ error: 'Token invalid' });
+
+        const now = new Date();
+
+        if(now > user.passwordResetExpires)
+            return res.status(400).send({ error: 'Token expired, generate a new one' });
+
+        user.password = password;
+
+        await user.save();
+
+        res.send();
+
+    } catch (err) {
+        console.log(err);
+        res.status(400).send({ error: 'Cannot reset password, try again'});
+    }
+});
 
 module.exports = app => app.use('/auth', router);
--- src/app/controllers/projectController.js
+++ src/app/controllers/projectController.js
@@ -8,5 +8,4 @@ router.get('/', (req, res) => {
     res.send({ ok: true, user: req.userId});
 });
 
-
 module.exports = app => app.use('/projects', router);
